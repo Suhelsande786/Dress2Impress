20f1d7d5017e18fd7ddcaa6041696b1e4de88793
Suhel Sande <suhelsande2015@gmail.com>
2019-02-05 11:10:34 +0530

    My Project Setup Completed Successfully

---
+++ Dress2ImpressUltraBackEnd/src/main/java/com/ss/Dress2ImpressUltraBackEnd/App.java
@@ -0,0 +1,13 @@
+package com.ss.Dress2ImpressUltraBackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
